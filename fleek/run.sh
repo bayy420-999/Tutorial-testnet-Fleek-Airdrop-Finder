e5d07b08d29a04a0d1c1034b676da1a85834b509
Bayu Aji <getkilla5@gmail.com>
2022-12-08 21:29:44 +0700

    Add run.sh

---
+++ fleek/run.sh
@@ -0,0 +1,18 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install build-essential cmake curl clang pkg-config libssl-dev git ca-certificates gnupg lsb-release -y
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/ubuntu \
+  $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+sudo apt-get update
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+source "$HOME/.cargo/env"
+cargo install sccache
+git clone https://github.com/fleek-network/ursa.git
+cd ursa
+make install
+docker build -t ursa -f ./Dockerfile .
+docker run -p 4069:4069 --name ursa-cli -it ursa
